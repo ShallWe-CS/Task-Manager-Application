641cd518cc1bf9e72c429d202c97a822e2038f01
KNR1997 <kethakaranasinghe@gmail.com>
2024-02-29 16:05:20 +0530

    Models modified

--- TMA/base/admin.py
+++ TMA/base/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import CustomUser, Task, Board
+from .models import CustomUser, Task, Board, Column
 
 # Register your models here.
 admin.site.register(CustomUser)
 admin.site.register(Task)
 admin.site.register(Board)
+admin.site.register(Column)
--- TMA/base/models.py
+++ TMA/base/models.py
@@ -7,22 +7,20 @@ class CustomUser(AbstractUser):
         ('user', 'User'),
         ('leader', 'Leader'),
         ('admin', 'Admin'),
-        # Add more roles as needed
     )
-
-    # Add your custom fields here
-    # For example, you might add fields like profile_picture, address, etc.
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='customer')
-
-    # Add any other custom fields as needed
 
     def __str__(self):
         return self.username
     
 class Board(models.Model):
     name = models.CharField(max_length=255)
-    ownerId = models.IntegerField(default=1)
-    users = models.ManyToManyField(CustomUser)
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=1, related_name='owned_boards')
+    boards = models.ManyToManyField(CustomUser, related_name='users_joined')
+
+class Column(models.Model):
+    name = models.CharField(max_length=255)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, default=1)
 
 class Task(models.Model):
     title = models.CharField(max_length=255)
@@ -30,3 +28,4 @@ class Task(models.Model):
     status = models.CharField(max_length=255)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=1)
     board = models.ForeignKey(Board, on_delete=models.CASCADE, default=1)
+    column = models.ForeignKey(Column, on_delete=models.CASCADE, default=1)
